4c9f78d86523fdb6cbc6cc74f6d09092f3ac0ce4
leo_yang <{username}@users.noreply.github.com>
2017-09-12 17:07:55 +0800

    sql script

---
+++ mysql.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `t_userinfo` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8
+
+CREATE TABLE `t_userinfo` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8
